0c9e6f284934a43faef18b1602a73b6465f195f7
aybakaldaamsa <aybakaldaamsa2@gmail.com>
2024-01-06 16:41:57 +0300

    21

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -28,6 +28,6 @@ fi
 
 num_files=$(find "$filesdir" -type f| wc -l)
 num_matching_lines=$(grep -r "$searchstr" "$filesdir" | wc -l )
-echo "the number of files in $num_files and the number of matching lines are $num_matching_lines "
+echo "The number of files are $num_files and the number of matching lines are $num_matching_lines "
 
 
